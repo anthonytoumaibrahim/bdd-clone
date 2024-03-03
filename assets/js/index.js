3089ffb6e9e42d05560014f8dfee3a3484d3e239
atian155 <atianthony@outlook.com>
2024-03-03 12:33:47 +0200

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,36 @@
+const siteHeader = document.getElementById("site-header");
+const headerScroll = document.getElementById("header-scroll");
+const headerLogo = document.querySelector("#site-header .logo");
+const hero = document.querySelector(".hero");
+const scrollToTopButton = document.querySelector(".scroll-to-top");
+
+// Using an IntersectionObserver to make the header fixed after scroll
+const headerObserver = new IntersectionObserver(
+  (entries) =>
+    entries.forEach((entry) => {
+      siteHeader.classList.toggle("header-compact", !entry.isIntersecting);
+      siteHeader.classList.toggle("slideInDown", !entry.isIntersecting);
+      headerLogo.src = !entry.isIntersecting
+        ? "./assets/images/logo-compact.png"
+        : "./assets/images/logo.png";
+    }),
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: `${siteHeader.clientHeight}px`,
+  }
+);
+headerObserver.observe(headerScroll);
+
+// Using an IntersectionObserver to show the scroll to top button
+const scrollToTopObserver = new IntersectionObserver(
+  (entries) =>
+    entries.forEach((entry) =>
+      scrollToTopButton.classList.toggle("hide", entry.isIntersecting)
+    ),
+  {
+    root: null,
+    threshold: 0.7,
+  }
+);
+scrollToTopObserver.observe(hero);
